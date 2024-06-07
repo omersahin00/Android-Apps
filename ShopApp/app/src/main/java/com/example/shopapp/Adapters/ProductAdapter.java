eae52bee64cc61d06b3b7033a254a872a709488b
Ömer Şahin <omersahin0309@gmail.com>
2024-06-08 02:07:38 +0300

    Feature: Innovations were introduced to the UI.

--- ShopApp/app/src/main/java/com/example/shopapp/Adapters/ProductAdapter.java
+++ ShopApp/app/src/main/java/com/example/shopapp/Adapters/ProductAdapter.java
@@ -1,5 +1,9 @@
 package com.example.shopapp.Adapters;
 
+import static android.content.ContentValues.TAG;
+
+import android.annotation.SuppressLint;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +13,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.shopapp.Helpers.FirebaseDatabaseHelper;
+import com.example.shopapp.Models.Brand;
 import com.example.shopapp.Models.Product;
 import com.example.shopapp.R;
 
@@ -18,6 +24,7 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHold
 
     private List<Product> mProductList;
     private OnItemClickListener mListener;
+    private Brand brand;
 
     public interface OnItemClickListener {
         void onItemClick(int position);
@@ -42,6 +49,21 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHold
         holder.productPrice.setText(String.valueOf(product.getPrice()) + " TL");
         holder.productImage.setImageResource(product.getImageResource());
 
+        FirebaseDatabaseHelper<Brand> brandFirebaseDatabaseHelper = new FirebaseDatabaseHelper<>(Brand.class, "brands");
+        brandFirebaseDatabaseHelper.getOneData(new FirebaseDatabaseHelper.DataListener<Brand>() {
+            @Override
+            public void onDataReceived(Brand data) {
+                if (data != null) {
+                    brand = data;
+                    holder.brandImage.setImageResource(brand.getImageResource());
+                }
+            }
+            @Override
+            public void onError(Exception e) {
+                Log.e(TAG, "onError: ", e);
+            }
+        }, product.getBrandName(), "name");
+
         // Tıklama olayı ekleniyor
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,12 +84,15 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHold
         ImageView productImage;
         TextView productName;
         TextView productPrice;
+        ImageView brandImage;
 
+        @SuppressLint("WrongViewCast")
         public ViewHolder(View itemView) {
             super(itemView);
             productImage = itemView.findViewById(R.id.productImage);
             productName = itemView.findViewById(R.id.productName);
             productPrice = itemView.findViewById(R.id.productPrice);
+            brandImage = itemView.findViewById(R.id.brandImage);
         }
     }
 }
